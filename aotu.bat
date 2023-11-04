215e8911446ac511f622cc658911e831d8d5853d
HAppY <1144535569@qq.com>
2023-11-04 20:44:41 +0800

    Auto commit

--- aotu.bat
+++ aotu.bat
@@ -4,9 +4,9 @@ set matchStr=443
 :start
 git add .
 git commit -m "Auto commit"
-git push origin master > output.txt
-set /p str=<output.txt
-echo %str% | findstr %matchStr% >nul && goto start || echo "push ok"
-sleep 1800 # 每隔1小时自动提交一次
+git push origin master
+@REM set /p str=<output.txt
+@REM echo %str% | findstr %matchStr% >nul && goto start || echo "push ok"
+sleep 10
 
 goto start
